acb5e582b9bb475e766b6ae0082b85d79a76dcb2
AppleSK <appleskhd@gmail.com>
2021-04-10 22:10:34 +0200

    Added search command

---
+++ commands/search.js
@@ -0,0 +1,79 @@
+const PROFILE_MODEL = require("../models/profileSchema");
+
+module.exports = {
+  name: "search",
+  permissions: [],
+  cooldown: 30,
+  description: "Choose your search location and have a chance at some bars!",
+  execute(client, message, args, Discord, profileData) {
+    const LOCATIONS = [
+      "car",
+      "sock",
+      "milk",
+      "wallet",
+      "box",
+      "pocket",
+      "bus",
+      "gutters",
+      "park",
+      "train",
+      "lounge",
+      "keyboard",
+      "picnic",
+      "bathroom",
+      "bed",
+      "sofa",
+      "backpack",
+      "laptop",
+      "oculus",
+      "shirt",
+    ];
+
+    let chosenLocations = LOCATIONS.sort(() => Math.random() - Math.random()).slice(0, 3);
+
+    const RANDOM_NUMBER = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+
+    const FILTER = (m) => {
+      return chosenLocations.some((answer) => answer.toLowerCase() === m.content.toLowerCase()) && m.author.id === message.author.id;
+    };
+
+    const COLLECTOR = message.channel.createMessageCollector(FILTER, { max: 1, time: 15000 });
+
+    COLLECTOR.on("collect", async (m) => {
+      const EMBED = new Discord.MessageEmbed()
+        .setColor("#207144'")
+        .setTitle(`${message.author.username} searched a ${m.content}!`)
+        .setDescription(`You found ${RANDOM_NUMBER.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}<:HPbar:830500268089147424>`)
+
+      await PROFILE_MODEL.findOneAndUpdate(
+        {
+          userID: message.author.id,
+        },
+        {
+          $inc: {
+            bars: RANDOM_NUMBER,
+          },
+        }
+      );
+
+      message.channel.send(EMBED);
+    });
+
+    COLLECTOR.on("end", (collected) => {
+      if (collected.size == 0) {
+        return message.channel.send(
+          `What are you doing <@${message.author.id}>?! There was ${RANDOM_NUMBER.toString().replace(
+            /\B(?=(\d{3})+(?!\d))/g,
+            ","
+          )}<:HPbar:830500268089147424>hidden inside the ${chosenLocations[0]}!`
+        );
+      }
+    });
+
+    message.channel.send(
+      `<@${
+        message.author.id
+      }>\n**Which location would you like to search?** <:HPsearch:830534770420875275>\nType the location in this channel.\n\`${chosenLocations.join("` `")}\``
+    );
+  },
+};
