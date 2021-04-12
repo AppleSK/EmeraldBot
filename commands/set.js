f989e5f7e14dcd1c46f079f454b12e0adcfd3f5b
AppleSK <appleskhd@gmail.com>
2021-04-12 17:37:41 +0200

    Deleted give and remove, adjusted work

--- commands/set.js
+++ commands/set.js
@@ -3,12 +3,12 @@ module.exports = {
   name: "set",
   permissions: ["ADMINISTRATOR"],
   cooldown: 10,
-  description: "Set bars of a member!",
+  description: "Set bars FOR a member!",
   async execute(client, message, args, Discord, profileData) {
     const error = new Discord.MessageEmbed() 
         .setColor('207144')
         .setTitle('It looks like there was an error! Please use the command like stated down below!')
-        .setDescription('`(prefix)set, user(with @, must have a profile), amount`')  
+        .setDescription('`(prefix)set, user(with @, must have a profile), amount(must be more than 0)`')  
     if (!args.length) return message.channel.send(error);
     const amount = args[1];
     const target = message.mentions.users.first();
@@ -29,12 +29,12 @@ module.exports = {
           },
         }
       );
-      const GIVEEMBED = new Discord.MessageEmbed() 
+      const SETEM = new Discord.MessageEmbed() 
         .setColor('207144')
-        .setTitle('Give')
-        .setDescription(`Sucessfully set ${amount}<:HPbar:830500268089147424>!`) 
+        .setTitle('Set')
+        .setDescription(`Sucessfully set to ${amount}<:HPbar:830500268089147424>!`) 
 
-      return message.channel.send(GIVEEMBED);
+      return message.channel.send(SETEM);
     } catch (err) {
       console.log(err);
     }
--- commands/work.js
+++ commands/work.js
@@ -10,15 +10,14 @@ async execute(client, message, args, Discord, profileData) {
 
     const user = message.author;
 
-        const replies = ['Programmer','Builder','Waiter','Busboy','Chief','Mechanic', 'Gamer']
+        const replies = ['programmer','builder','waiter','driver','chef','mechanic', 'gamer', 'YouTuber', 'teacher', 'artist', 'developer', 'doctor', 'nurse', 'dentist', 'scientist', 'pilot', 'engineer', 'lawyer', 'manager', 'architect', 'plumber', 'firefighter']
 
         const result = Math.floor((Math.random() * replies.length));
         const amount = Math.floor(Math.random() * 250) + 1;
 
-
         const embed1 = new Discord.MessageEmbed()
         .setTitle("Work")
-        .setDescription(`You worked as a ${replies[result]} and earned ${amount}<:HPbar:830500268089147424>`)
+        .setDescription(`You worked as \`${replies[result]}\` and earned ${amount}<:HPbar:830500268089147424>`)
         .setColor("207144")
         message.channel.send(embed1)
         
