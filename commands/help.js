916743eeb1234190f974d17d44a8cc1b1915ede5
AppleSK <appleskhd@gmail.com>
2021-04-14 09:13:22 +0200

    Fixed search and changed help

--- commands/help.js
+++ commands/help.js
@@ -11,17 +11,17 @@ module.exports = {
         .setDescription('Every command and what it does!')
         .addFields(
         {name: 'bal', value: 'Tells you how much you have in your wallet and how much you have in the bank!'},
-        {name: 'beg', value: 'Beg for some bars and you will get a random amount!'},
-        {name: 'withdraw', value: 'Withdraw bars from your bank!'},
-        {name: 'deposit', value: 'Deposit bars into your bank!'},
-        {name: 'search', value: 'Search a location for some bars!'},
+        {name: 'beg', value: 'Beg for some emeralds and you will get a random amount!'},
+        {name: 'withdraw', value: 'Withdraw emeralds from your bank!'},
+        {name: 'deposit', value: 'Deposit emeralds into your bank!'},
+        {name: 'search', value: 'Search a location for some emeralds!'},
         {name: 'icon', value: 'Get an icon of a member!'},
-        {name: 'daily', value: 'Get bars everyday!'},
-        {name: 'hourly', value: 'Get bars every hour!'},
-        {name: 'crime', value: 'Commit a crime and you will either lose or gain money.'},
-        {name: 'gamble', value: 'Gamble with an amount of bars of your choice and you will get double or lose half of it!'},
-        {name: 'work', value: 'Go to work and get some bars!'},
-        {name: 'pay', value: 'Gift some bars to a member!'},
+        {name: 'daily', value: 'Get emeralds everyday!'},
+        {name: 'hourly', value: 'Get emeralds every hour!'},
+        {name: 'crime', value: 'Commit a crime and you will either lose or gain emeralds.'},
+        {name: 'gamble', value: 'Gamble with an amount of emeralds of your choice and you will get double or lose half of it!'},
+        {name: 'work', value: 'Go to work and get some emeralds!'},
+        {name: 'pay', value: 'Gift some emeralds to a member!'},
         )
 
         message.channel.send(helpEmbed);
--- commands/search.js
+++ commands/search.js
@@ -2,9 +2,9 @@ const PROFILE_MODEL = require("../models/profileSchema");
 
 module.exports = {
   name: "search",
-  permissions: [],
-  cooldown: 30,
-  description: "Choose your search location and have a chance at some bars!",
+  permissions: ["SEND_MESSAGES"],
+  cooldown: 15,
+  description: "Choose your search location and have a chance at some emeralds!",
   execute(client, message, args, Discord, profileData) {
     const LOCATIONS = [
       "car",
@@ -31,7 +31,7 @@ module.exports = {
 
     let chosenLocations = LOCATIONS.sort(() => Math.random() - Math.random()).slice(0, 3);
 
-    const RANDOM_NUMBER = Math.floor(Math.random() * (150 - 100 + 1)) + 20;
+    const RANDOM_NUMBER = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
 
     const FILTER = (m) => {
       return chosenLocations.some((answer) => answer.toLowerCase() === m.content.toLowerCase()) && m.author.id === message.author.id;
@@ -42,9 +42,10 @@ module.exports = {
     COLLECTOR.on("collect", async (m) => {
       const EMBED = new Discord.MessageEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL())
-        .setColor("#30d56b'")
-        .setTitle(`${message.author.username} searched a ${m.content}!`)
+        .setColor("#30d56b")
+        .setTitle(`Search`)
         .setDescription(`You found ${RANDOM_NUMBER.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}<:HPemerald:831588273796415489>`)
+
       await PROFILE_MODEL.findOneAndUpdate(
         {
           userID: message.author.id,
@@ -58,26 +59,18 @@ module.exports = {
 
       message.channel.send(EMBED);
     });
-    const NORESPONSE = new Discord.MessageEmbed()
-    .setAuthor(message.author.tag, message.author.avatarURL())
-    .setColor("#30d56b'")
-    .setTitle('Search')
-    .setDescription(`There was ${RANDOM_NUMBER.toString().replace(/\B(?=(\d{3})+(?!\d))/g,
-    ","
-    )}<:HPemerald:831588273796415489> hidden inside \n\`${chosenLocations[0]}\`!`)
 
     COLLECTOR.on("end", (collected) => {
       if (collected.size == 0) {
-        return message.channel.send(NORESPONSE)
-        
+        return
       }
+    });
     const SEARCHEMBED = new Discord.MessageEmbed()
     .setAuthor(message.author.tag, message.author.avatarURL())
     .setColor("#30d56b'")
     .setTitle('Where would you like to search?')
     .setDescription(`\nType the location in this channel.\n\`${chosenLocations.join("` `")}\``)
 
-    message.channel.send(SEARCHEMBED);
+    message.channel.send(SEARCHEMBED)
   },
-    )}
-}
+};
